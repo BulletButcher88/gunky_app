8b091f511eeff36824102585b7f89fb69c240099
Andrew Sims <acsims@tuta.io>
2019-06-19 19:53:23 +1000

    Simplify server launch file
    There's no need to do all the complicated stuff that you do in that
    file, I was a bit confused! All you need to do is initialise the express
    server and make it listen. Only one import is required.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.is')(server);
+const express = require('express');
+const server = express();
+port = 3000;
 
-server.listen(300)
-    // https://socket.io/docs/
+server.listen(3000, () => console.log(`Listening on port ${port}...`))
+
+// var app = require('express')();
+// var server = require('http').Server(app);
+// var io = require('socket.is')(server);
+
+// server.listen(300)
+//     // https://socket.io/docs/
