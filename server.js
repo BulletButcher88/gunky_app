0fd53d73023302ea74b4321b20571965d531a520
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-06-21 10:39:25 +1000

    Install CORS, write outline of server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.is')(server);
+// Import the dependencies
+const express = require('express');
+const cors = require('cors')
 
-server.listen(300)
-    // https://socket.io/docs/
+// Define server
+const server = express();
+const port = 3000;
+
+// Use middleware to allow CORS, and transmission of JSON and form data.
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded());
+
+server.listen(port, () => {
+    `Listening on port ${port}...`
+})
