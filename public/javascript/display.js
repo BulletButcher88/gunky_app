12f2668ca61a48a710cf822b03ffa93e530d450d
Bull-Butcher <markjamesbutcher@gmail.com>
2019-06-22 16:00:59 +1000

    reconnection of files

--- public/javascript/display.js
+++ public/javascript/display.js
@@ -1,4 +1,4 @@
 function remote() {
-    win2 = window.open("remote.html", "", "width=400,height=650,scrollbars")
+    win2 = window.open("./public/remote.html", "", "width=400,height=650,scrollbars")
     win2.creator = self
 }
