19801ed88ef76f6ed55e999cf88d493fabb45e39
Andrew Sims <acsims@tuta.io>
2019-06-24 19:27:28 +1000

    Comment controllerS

--- server/controllers.js
+++ server/controllers.js
@@ -1,8 +1,11 @@
+// Push the received data from the controller to the queue.
 const audienceController = (req, res) => {
   musicData.push(req.body);
   console.log(`Current queue for band: ${queue}`);
 }
 
+// Send the next item in the queue, remove it from the queue,
+// and print the new queue to the server logs.
 const bandController = (req, res) => {
   res.send(queue[0]);
   musicData = musicData.slice(1);
