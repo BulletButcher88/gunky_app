f8860ccf883dd61d980056320ed9a50c843531b0
Andrew Sims <acsims@tuta.io>
2019-06-24 19:30:20 +1000

    Comment routes and add new ones for accessing HTML

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { audienceController, bandController } = require('./controllers')
+const { audiencePage, audienceData, bandPage, bandData } = require('./controllers')
 
 const router = new express.Router();
 
-router.post('/', audienceController);
-router.get('/band', bandController);
+// First endpoint sends the HTML page for the band
+// Second endpoint is where the JS posts new data for queueing
+router.get('/', audiencePage)
+router.post('/', audienceData);
+
+// First endpoint sends the HTML page for the band
+// Second endpoint is where the JS requests the next item in queue
+router.get('/band', bandPage);
+router.get('/band-next-item', bandData)
 
 module.exports = router;
