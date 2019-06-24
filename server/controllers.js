4d884d8842ef2bf29bbb20027b803c138a4fe860
Andrew Sims <acsims@tuta.io>
2019-06-24 19:32:10 +1000

    Add controllers to present HTML to band and audience

--- server/controllers.js
+++ server/controllers.js
@@ -1,18 +1,28 @@
+const audiencePage = (req, res) => {
+  res.send("This will be a webpage one day.")
+}
+
 // Push the received data from the controller to the queue.
-const audienceController = (req, res) => {
+const audienceData = (req, res) => {
   musicData.push(req.body);
   console.log(`Current queue for band: ${queue}`);
 }
 
+const bandPage = (req, res) => {
+  res.send("This will be a webpage one day.")
+}
+
 // Send the next item in the queue, remove it from the queue,
 // and print the new queue to the server logs.
-const bandController = (req, res) => {
+const bandData = (req, res) => {
   res.send(queue[0]);
   musicData = musicData.slice(1);
   console.log(`Current queue for band: ${queue}`);
 }
 
 module.exports = {
-  audienceController,
-  bandController
+  audiencePage,
+  audienceData,
+  bandPage,
+  bandData
 }
